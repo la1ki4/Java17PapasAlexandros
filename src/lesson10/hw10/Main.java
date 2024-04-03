9c8043075696c66863e65e20c91c483c934a6c7e
la1ki4 <alexandr.papas@mail.ru>
2024-04-03 11:52:54 +0300

    added homework 10

---
+++ src/lesson10/hw10/Main.java
@@ -0,0 +1,20 @@
+package lesson10.hw10;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void fillingPerson(Person somePerson){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter person's name: ");
+        somePerson.setName(scanner.next());
+        System.out.print("Enter person's age: ");
+        somePerson.setAge(scanner.nextByte());
+        System.out.print("Enter person's height: ");
+        somePerson.setHeight(scanner.nextFloat());
+    }
+    public static void main(String[] args) {
+        Person vasea = new Person();
+        fillingPerson(vasea);
+        vasea.printPersonInfo();
+    }
+}
