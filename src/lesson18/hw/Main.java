837b4d17c1eb611d61666fc3a271ef78213e0f7f
Alexandros <alexandr.papas@mail.ru>
2024-04-21 11:37:41 +0300

    removed condition with instanceof catch blockes was been separated

--- src/lesson18/hw/Main.java
+++ src/lesson18/hw/Main.java
@@ -7,15 +7,12 @@ public class Main {
     public static void main(String[] args) {
         Car car = new Car("Lamborghini", "red");
         try {
-            car.drive(0);
+            car.drive(1);
             car.safetyBelt("Not Belt");
-        } catch (NotFuelException | NotBeltException exception){
-            if(exception instanceof NotFuelException){
-                System.err.println(exception.getMessage());
-            }
-            if(exception instanceof NotBeltException){
-                System.err.println(exception.getMessage());
-            }
+        } catch (NotFuelException notFuelException){
+            System.err.println(notFuelException.getMessage());
+        } catch (NotBeltException notBeltException){
+            System.err.println(notBeltException.getMessage());
         }
     }
 }
